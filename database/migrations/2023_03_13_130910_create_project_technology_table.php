cdd4c0917c3b9d868ca95e32cd6f11b8aa1bd2ce
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-03-13 14:36:17 +0100

    added corelation on pivot

--- database/migrations/2023_03_13_130910_create_project_technology_table.php
+++ database/migrations/2023_03_13_130910_create_project_technology_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->foreignId('technology_id')->constrained()->onDelete('cascade');
         });
     }
 
